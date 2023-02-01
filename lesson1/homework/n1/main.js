6f1537559abe850176e50a863849e5d676ba9d5e
IrynaKrasnova <irene.ksn@gmail.com>
2023-02-01 08:06:25 +0200

    tasks

---
+++ lesson1/homework/n1/main.js
@@ -0,0 +1,41 @@
+let a = 'hello';
+console.log(a);
+let b = 'owu';
+console.log(b);
+let c = 'com';
+console.log(c);
+let d = 16;
+console.log(d);
+let e = 1;
+console.log(e);
+let f = 10;
+console.log(f);
+let g = -999;
+console.log(g);
+let h = 123;
+console.log(h);
+const I = 3.14;
+console.log(I);
+let j = 2.7;
+console.log(j);
+let k = 16;
+console.log(k);
+let l = true;
+console.log(l);
+let m = false;
+console.log(m);
+
+
+let  firstName = 'Iryna';
+console.log(firstName);
+let  middleName = 'Serhiivna';
+console.log(middleName);
+let  lastName = 'Krasnova';
+console.log(lastName)
+
+let person1 = firstName + ' ' +middleName + ' '+lastName
+console.log(person1)
+
+
+let person2 = `${firstName} ${middleName} ${lastName}`
+console.log(person2)
