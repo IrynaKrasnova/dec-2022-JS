eb5d9228e3edf7d04fc5e0d9bb7f387396283ee7
IrynaKrasnova <irene.ksn@gmail.com>
2023-02-05 09:16:48 +0200

    tasks1

---
+++ lesson1/lesson2/tasks/main.js
@@ -0,0 +1,23 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+let months = ['January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October'];
+console.log(months);
+console.log(months[0]);
+console.log(months[1]);
+console.log(months[2]);
+console.log(months[3]);
+console.log(months[4]);
+console.log(months[5]);
+console.log(months[6]);
+console.log(months[7]);
+console.log(months[8]);
+console.log(months[9]);
