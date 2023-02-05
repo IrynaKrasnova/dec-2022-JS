5168400ef296fcac9fe1eaaadc0b0ad00be2c553
IrynaKrasnova <irene.ksn@gmail.com>
2023-02-05 11:40:29 +0200

    tasks2(1)

--- lesson1/lesson2/tasks/two/main.js
+++ lesson1/lesson2/tasks/two/main.js
@@ -31,3 +31,25 @@ let books={
 console.log(books);
 
 // - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+users=[ {name:'Irina', username: 'one', password: 'ooo'},
+    {name:'Artem', username: 'two', password: 'sss'},
+    {name:'Olga', username: 'three', password: 'yyy'},
+    {name:'Julia', username: 'four', password: 'rpr'},
+    {name:'Volodymyr', username: 'five', password: 'hhh'},
+    {name:'Alex', username: 'six', password: 'pppp'},
+    {name:'Oleg', username: 'seven', password: 'ffffff'},
+    {name:'Jane', username: 'eight', password: 'ddd'},
+    {name:'Irene', username: 'nine', password: 'uuu'},
+    {name:'Eva', username: 'ten', password: 'ccc'},
+]
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
