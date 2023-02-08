3435f3fff212e315e252fe33af04930b89fc8db5
IrynaKrasnova <irene.ksn@gmail.com>
2023-02-08 13:38:14 +0200

    base2

--- lesson3/tasks/I/main.js
+++ lesson3/tasks/I/main.js
@@ -1,15 +1,134 @@
-let locations = [
-    {title: "м. Київ", type: "city"},
-    {title: "Житомирська область", type: "oblast"},
-    {title: "Харківська область", type: "oblast"},
-    {title: "Вінницька область", type: "oblast"},
-    {title: "Чернігівська область", type: "oblast"},
-    {title: "Дніпропетровська область", type: "oblast"},
-    {title: "Черкаська область", type: "oblast"},
-    {title: "Волинська область", type: "oblast"},
-    {title: "Київська область", type: "oblast"},
-    {title: "Миколаївська область", type: "oblast"},
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+// let locations = [
+//     {title: "м. Київ", type: "city"},
+//     {title: "Житомирська область", type: "oblast"},
+//     {title: "Харківська область", type: "oblast"},
+//     {title: "Вінницька область", type: "oblast"},
+//     {title: "Чернігівська область", type: "oblast"},
+//     {title: "Дніпропетровська область", type: "oblast"},
+//     {title: "Черкаська область", type: "oblast"},
+//     {title: "Волинська область", type: "oblast"},
+//     {title: "Київська область", type: "oblast"},
+//     {title: "Миколаївська область", type: "oblast"},
+// ];
+// for (let i=0; i<locations.length; i++){
+//     document.write(`
+//     <div>
+//         <h2>${locations[i].title}
+//         - ${locations[i].type}</h2>
+//     </div>`
+//     );
+// }
+
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+
+
+
+let usersList= [
+    {
+        name: 'Leanne Graham',
+        username: 'Bret',
+        company: {
+            name: 'Romaguera-Crona',
+            catchPhrase: 'Multi-layered client-server neural-net',
+            bs: 'harness real-time e-markets'
+        }
+    },
+    {
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        company: {
+            name: 'Deckow-Crist',
+            catchPhrase: 'Proactive didactic contingency',
+            bs: 'synergize scalable supply-chains'
+        }
+    },
+    {
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        company: {
+            name: 'Romaguera-Jacobson',
+            catchPhrase: 'Face to face bifurcated interface',
+            bs: 'e-enable strategic applications'
+        }
+    },
+    {
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        company: {
+            name: 'Robel-Corkery',
+            catchPhrase: 'Multi-tiered zero tolerance productivity',
+            bs: 'transition cutting-edge web services'
+        }
+    },
+    {
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        company: {
+            name: 'Keebler LLC',
+            catchPhrase: 'User-centric fault-tolerant solution',
+            bs: 'revolutionize end-to-end systems'
+        }
+    },
+    {
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo_Corkery',
+        company: {
+            name: 'Considine-Lockman',
+            catchPhrase: 'Synchronised bottom-line interface',
+            bs: 'e-enable innovative applications'
+        }
+    },
+    {
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.Skiles',
+        company: {
+            name: 'Johns Group',
+            catchPhrase: 'Configurable multimedia task-force',
+            bs: 'generate enterprise e-tailers'
+        }
+    },
+    {
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Maxime_Nienow',
+        company: {
+            name: 'Abernathy Group',
+            catchPhrase: 'Implemented secondary concept',
+            bs: 'e-enable extensible e-tailers'
+        }
+    },
+    {
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        company: {
+            name: 'Yost and Sons',
+            catchPhrase: 'Switchable contextually-based project',
+            bs: 'aggregate real-time technologies'
+        }
+    },
+    {
+        name: 'Clementina DuBuque',
+        username: 'Moriah.Stanton',
+        company: {
+            name: 'Hoeger LLC',
+            catchPhrase: 'Centralized empowering task-force',
+            bs: 'target end-to-end models'
+        }
+    }
 ];
-for (let i=0; i<locations.length; i++){
-    document.write(`<h3>${locations[0].title}</h3>`);
+for (let i = 0; i < usersList.length; i++) {
+    document.write(`
+    <div>
+    <h2>${usersList[i].name} - ${usersList[i].username}</h2>
+    </div>`
+    );
 }
+
+// for (let usersListElement of usersList) {
+//
+//     let company = usersListElement.company;
+//     console.log(company);
+//     for (let companyElement of company) {
+//         console.log(company);
+//     }
+// }
---
+++ lesson3/tasks/II/main.js
@@ -0,0 +1,57 @@
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+
+// let users = [
+//     {name: 'vasya', age: 31},
+//     {name: 'petya', age: 30},
+//     {name: 'kolya', age: 29},
+//     {name: 'olya', age: 28},
+//     {name: 'max', age: 30},
+//     {name: 'anya', age: 31},
+//     {name: 'oleg', age: 28},
+//     {name: 'andrey', age: 29},
+//     {name: 'masha', age: 30},
+//     {name: 'olya', age: 35},
+//     {name: 'nina', age: 38},
+//     {name: 'lena', age: 41},
+//     {name: 'oleg', age: 25},
+//     {name: 'katya', age: 29},
+//     {name: 'sima', age: 30},
+//     {name: 'julia', age: 40},
+//     {name: 'senya', age: 25},
+//     {name: 'omar', age: 39},
+//     {name: 'sam', age: 42},
+//     {name: 'nick', age: 37},
+// ];
+// let i = 0;
+// while (i<users.length) {
+//     document.write(`
+//     <h1>${users[i].name} - ${users[i].age}</h1>
+//     `);
+// i++;
+// }
+
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+
+let users = [
+    {name: 'vasya', age: 31, modules: ['html', 'css', 'js', 'mysql', 'git', 'java core', 'java advanced']},
+    {name: 'petya', age: 30, modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'git', 'java core', 'java advanced']},
+    {name: 'kolya', age: 29, modules: ['html', 'css', 'js', 'mysql',  'docker', 'git', 'java core', 'java advanced']},
+    {name: 'olya', age: 28, modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'git', 'java core', 'java advanced']},
+    {name: 'max', age: 30, modules: ['html', 'css',  'git', 'java core', 'java advanced']},
+    {name: 'anya', age: 31, modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'git', 'java core', 'java advanced']},
+    {name: 'oleg', age: 28, modules: ['html', 'css', 'java core', 'java advanced']},
+    {name: 'andrey', age: 29, modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'git', 'java core', 'java advanced']},
+    {name: 'masha', age: 30, modules: ['html', 'css', 'java core', 'java advanced']},
+    {name: 'olya', age: 35, modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'git', 'java core', 'java advanced']},
+    {name: 'nina', age: 38, modules: ['html', 'css', 'java advanced']},
+    {name: 'lena', age: 41, modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'git', 'java core', 'java advanced']},
+    {name: 'oleg', age: 25, modules: ['html', 'css', 'js', 'java core', 'java advanced']},
+    {name: 'katya', age: 29, modules: ['html', 'css', 'js',  'aws', 'docker', 'git', 'java core', 'java advanced']},
+    {name: 'sima', age: 30, modules: ['html', 'css', 'js', 'mongodb', 'angular', 'aws', 'docker', 'git', 'java core', 'java advanced']},
+    {name: 'julia', age: 40, modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular',  'java core', 'java advanced']},
+    {name: 'senya', age: 25, modules: ['html', 'css', 'js', 'mysql', 'java advanced']},
+    {name: 'omar', age: 39, modules: ['html', 'css', 'js', 'mysql', 'java core', 'java advanced']},
+    {name: 'sam', age: 42, modules: ['html', 'css', 'js', 'mysql',  'java core', 'java advanced']},
+    {name: 'nick', age: 37, modules: ['html', 'css', 'js', 'java advanced']},
+];
+
