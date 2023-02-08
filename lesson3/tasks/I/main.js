67876847e3a4096899fcc7b2e6b2227065fbfcfd
IrynaKrasnova <irene.ksn@gmail.com>
2023-02-08 09:48:57 +0200

    base

---
+++ lesson3/tasks/I/main.js
@@ -0,0 +1,15 @@
+let locations = [
+    {title: "м. Київ", type: "city"},
+    {title: "Житомирська область", type: "oblast"},
+    {title: "Харківська область", type: "oblast"},
+    {title: "Вінницька область", type: "oblast"},
+    {title: "Чернігівська область", type: "oblast"},
+    {title: "Дніпропетровська область", type: "oblast"},
+    {title: "Черкаська область", type: "oblast"},
+    {title: "Волинська область", type: "oblast"},
+    {title: "Київська область", type: "oblast"},
+    {title: "Миколаївська область", type: "oblast"},
+];
+for (let i=0; i<locations.length; i++){
+    document.write(`<h3>${locations[0].title}</h3>`);
+}
