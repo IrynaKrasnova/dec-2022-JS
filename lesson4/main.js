c44107bc04fab894072339fd178aed957c9c2e1f
IrynaKrasnova <irene.ksn@gmail.com>
2023-02-13 18:21:53 +0200

    part1

--- lesson4/main.js
+++ lesson4/main.js
@@ -23,6 +23,62 @@
 // li(``, `Irene`, `Vova`, `Artem`)
 
 
+// - створити функцію яка приймає масив та виводить кожен його елемент
 
 
-    
+// let usersList= [
+//     {name: 'Leanne Graham', username: 'Bret'},
+//     {name: 'Clementine Bauch', username: 'Samantha'},
+//     {name: 'Patricia Lebsack', username: 'Karianne'},
+//     {name: 'Chelsey Dietrich', username:'Maxime'},
+// ];
+//
+// function looper(arr) {
+//     for (const arrElement of arr) {
+//         console.log(arrElement);
+//
+//     }
+//
+// }
+// looper(usersList)
+
+// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+
+    // function square(a,b) {
+    // console.log(a*b)
+    //
+    // }
+    // square(5,10)
+
+// - створити функцію яка обчислює та повертає площу кола з радіусом r
+
+//
+// function radius() {
+//     let radius = +prompt('enter your radius, cm')
+//     document.write(3.14*radius)
+//
+// }
+// radius()
+
+
+// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+
+// function cylinder() { let r = +prompt('enter your radius, cm')
+//     let h = +prompt('enter your height, cm')
+//
+//     document.write((2*3.14*r)*(r+h))
+//
+// }
+//
+// cylinder()
+
+
+
+
+
+
+
+
+
+
+
