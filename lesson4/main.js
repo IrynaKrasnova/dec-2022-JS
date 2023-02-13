c70b39ce797e8c5334d1e421f890da7ccd361150
IrynaKrasnova <irene.ksn@gmail.com>
2023-02-13 17:26:35 +0200

    base

---
+++ lesson4/main.js
@@ -0,0 +1,28 @@
+// - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+//
+// function paragraph(words) {
+//     let text = `<p>
+// ${words}
+// </p>`;
+//     document.write(text);
+// }
+// paragraph(`hello`)
+
+
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+
+// function li(title,one,two,three) {
+//     let text = `<h3>${title}</h3>
+// <ul>
+//     <li>${one}</li>
+//     <li>${two}</li>
+//     <li>${three}</li>
+// </ul>`;
+//     document.write(text);
+// }
+// li(``, `Irene`, `Vova`, `Artem`)
+
+
+
+
+    
