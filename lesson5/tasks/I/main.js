4b26cc50f0b8d12276f00f7c931e7af0099e39f4
IrynaKrasnova <irene.ksn@gmail.com>
2023-02-15 10:03:37 +0200

    base

---
+++ lesson5/tasks/I/main.js
@@ -0,0 +1,26 @@
+// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+
+//
+// let square = (a,b)=>a*b;
+// console.log(square(5,10));
+
+
+// - створити функцію яка обчислює та повертає площу кола з радіусом r
+//
+// let S = (r)=>3.14*r*r;
+// document.write(S(50));
+
+// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+//
+// let cylinderS = (h,r)=>(2*3.14*r)*(r+h);
+// console.log(cylinderS(10,20));
+
+
+// - створити функцію яка приймає масив та виводить кожен його елемент
+
+let usersList= [
+    {name: 'Leanne Graham', username: 'Bret'},
+    {name: 'Clementine Bauch', username: 'Samantha'},
+    {name: 'Patricia Lebsack', username: 'Karianne'},
+    {name: 'Chelsey Dietrich', username:'Maxime'},
+];
