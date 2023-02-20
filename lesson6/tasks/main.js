ff9ad30c64a0234ad002284f8a05578b068c28dc
IrynaKrasnova <irene.ksn@gmail.com>
2023-02-20 18:23:23 +0200

    base

---
+++ lesson6/tasks/main.js
@@ -0,0 +1,41 @@
+// - Знайти та вивести довижину наступних стрінгових значень
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+
+let s = 'hello world';
+let x = s.indexOf('hello world');
+console.log(x)
+
+
+
+
+// - Перевести до великого регістру наступні стрінгові значення
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+// let s = `hello world, lorem ipsum, javascript is cool`;
+// let big = s.toUpperCase();
+// console.log(big);
+
+
+// - Перевести до нижнього регістру настипні стрінгові значення
+// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+
+// let s = `HELLO WORLD, LOREM IPSUM, JAVASCRIPT IS COOL`;
+// let small = s.toLowerCase();
+// console.log(small)
+
+// - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+
+// let s = ' dirty string   ';
+// let clean = s.split(` `)
+// console.log(clean)
+// s.substring(0,[3,4,5])
+// console.log(s)
+
+
+
+
+
+
+
+
