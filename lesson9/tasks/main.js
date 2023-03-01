8d789f06a84384d3634e131b05ef7b60915549ee
IrynaKrasnova <irene.ksn@gmail.com>
2023-03-01 14:38:24 +0200

    base

--- lesson9/tasks/main.js
+++ lesson9/tasks/main.js
@@ -4,9 +4,37 @@
 // - додати цей блок в body.
 // - клонувати його повністю, та додати клон в body.
 
-let div = document.createElement(`div`);
-div.classList.add(`wrap`,`collapse`, `alpha`, `beta`);
-div.style.background = `silver`;
-document.body.appendChild(div);
-console.log(document);
+// let div = document.createElement("div");
+// div.innerText = `Hello`
+// document.body.appendChild(div);
+// div.classList.add(`wrap`,`collapse`, `alpha`, `beta`);
+// div.style.background = `silver`;
+// div.style.color = `red`;
+// div.style.fontSize = `200px`;
+//
+// document.body.appendChild(div.cloneNode(true));
 
+
+// - Є масив: ['Main','Products','About us','Contacts']
+// Зробити ul в середині якої будуть лежати елементи масиву (кожен в своєму li)
+
+// let arr = ['Main','Products','About us','Contacts'];
+// let ul = document.createElement(`ul`)
+// document.body.appendChild(ul);
+//
+// for (const items of arr) {
+//     let li = document.createElement(`li`);
+//     li.innerText = `${items}`;
+//     document.body.appendChild(li);
+// }
+
+
+
+
+
+// let arr = ['Main','Products','About us','Contacts'];
+//
+// for (const string of arr) {
+//     let ul = document.createElement(`ul`);
+//     let li = document.createElement(`li`);
+//     li.innerText = `${li};
