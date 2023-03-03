8daf700e094c236a053387065a94da8b72f410a6
IrynaKrasnova <irene.ksn@gmail.com>
2023-03-03 10:49:48 +0200

    II

--- lesson9/tasks/main.js
+++ lesson9/tasks/main.js
@@ -108,71 +108,177 @@
 // Проітерувати його, створиши для кожного об'єкту  масиву <div class='member'> та наповнити його данними з об'єкту.
 //     Якщо людською мовою: під кожного члена родини зробити блок та наповнити його інформацією з цього об'єкту
 
-let simpsons = [
-    {
- name: 'Bart',
-    surname: 'Simpson',
-    age: 10,
-    info: 'Бартолом\'ю ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.',
-    photo: 'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'
-  },
-  {
-    name: 'Homer',
-    surname: 'Simpson',
-    age: 40,
-    info: 'Гомер Джей Сімпсон (англ. Homer Jay Simpson) — один із головних героїв мультсеріалу «Сімпсони». Гомер — грубий і неввічливий батько родини, він має очевидні вади: товстий, лисий і не дуже розумний. Нерідко він поводиться як блазень, абсурдно, егоїстично і нетактовно, але все ж лишається симпатичним.',
-    photo: 'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'
-  },
-  {
-    name: 'Marge',
-    surname: 'Simpson',
-    age: 38,
-    info: 'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер. Обычно носит зелёное платье, красные балетки, на шее — ожерелье из искусственного жемчуга и ездит на оранжевом универсале. У неё шикарные синие волосы, которые она обычно собирает в очень высокую причёску. Глаза цвета ореха (19s6e). Основное занятие — домохозяйка, большую часть времени проводит в заботе о доме, детях и Гомере. Образ Мардж копирует стереотип провинциальной американской домохозяйки 50-х годов. Мардж — единственный член семьи, посещающий церковь добровольно. Старается поддерживать нравственность не только своей семьи, но и всего города. Отлично готовит, особенно славятся её свиные отбивные и зефир. Любимое блюдо — лапша с маслом.',
-    photo: 'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'
-  },
-  {
-    name: 'Lisa',
-    surname: 'Simpson',
-    age: 9,
-    info: 'Ли́за Мари́ Си́мпсон (англ. Lisa Marie Simpson) — героиня мультипликационного сериала «Симпсоны». Средний ребёнок в семье, восьмилетняя девочка, выделяющаяся среди остальных Симпсонов прежде всего своим умом и рассудительностью.',
-    photo: 'https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png'
-  },
-  {
-    name: 'Maggie',
-    surname: 'Simpson',
-    age: 1,
-    info: 'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Впервые появилась на телевидении в шоу Трейси Ульман, в короткометражке Good Night (англ.)русск. 19 апреля 1987 года. Мэгги была придумана и разработана карикатуристом Мэттом Грейнингом, пока он ждал встречи с Джеймсом Л. Бруксом. Названа в честь младшей сестры Грейнинга. После появления в шоу Трейси Ульман, через три года семья Симпсонов получила собственный сериал на телеканале Fox, дебют произошёл 17 декабря 1989 года.',
-    photo: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'
-  },
-];
-const body = document.querySelector('body');
-for (const simpson of simpsons) {
+// let simpsons = [
+//     {
+//  name: 'Bart',
+//     surname: 'Simpson',
+//     age: 10,
+//     info: 'Бартолом\'ю ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.',
+//     photo: 'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'
+//   },
+//   {
+//     name: 'Homer',
+//     surname: 'Simpson',
+//     age: 40,
+//     info: 'Гомер Джей Сімпсон (англ. Homer Jay Simpson) — один із головних героїв мультсеріалу «Сімпсони». Гомер — грубий і неввічливий батько родини, він має очевидні вади: товстий, лисий і не дуже розумний. Нерідко він поводиться як блазень, абсурдно, егоїстично і нетактовно, але все ж лишається симпатичним.',
+//     photo: 'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png',
+//   },
+//   {
+//     name: 'Marge',
+//     surname: 'Simpson',
+//     age: 38,
+//     info: 'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер. Обычно носит зелёное платье, красные балетки, на шее — ожерелье из искусственного жемчуга и ездит на оранжевом универсале. У неё шикарные синие волосы, которые она обычно собирает в очень высокую причёску. Глаза цвета ореха (19s6e). Основное занятие — домохозяйка, большую часть времени проводит в заботе о доме, детях и Гомере. Образ Мардж копирует стереотип провинциальной американской домохозяйки 50-х годов. Мардж — единственный член семьи, посещающий церковь добровольно. Старается поддерживать нравственность не только своей семьи, но и всего города. Отлично готовит, особенно славятся её свиные отбивные и зефир. Любимое блюдо — лапша с маслом.',
+//     photo: 'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'
+//   },
+//   {
+//     name: 'Lisa',
+//     surname: 'Simpson',
+//     age: 9,
+//     info: 'Ли́за Мари́ Си́мпсон (англ. Lisa Marie Simpson) — героиня мультипликационного сериала «Симпсоны». Средний ребёнок в семье, восьмилетняя девочка, выделяющаяся среди остальных Симпсонов прежде всего своим умом и рассудительностью.',
+//     photo: 'https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png'
+//   },
+//   {
+//     name: 'Maggie',
+//     surname: 'Simpson',
+//     age: 1,
+//     info: 'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Впервые появилась на телевидении в шоу Трейси Ульман, в короткометражке Good Night (англ.)русск. 19 апреля 1987 года. Мэгги была придумана и разработана карикатуристом Мэттом Грейнингом, пока он ждал встречи с Джеймсом Л. Бруксом. Названа в честь младшей сестры Грейнинга. После появления в шоу Трейси Ульман, через три года семья Симпсонов получила собственный сериал на телеканале Fox, дебют произошёл 17 декабря 1989 года.',
+//     photo: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'
+//   },
+// ];
+// const body = document.querySelector('body');
+// for (const simpson of simpsons) {
+//
+//     let div = document.createElement('div');
+//     let h2 = document.createElement('h2');
+//     let i = document.createElement('i');
+//     let p = document.createElement('p');
+//     let img = document.createElement('img');
+//
+//
+//     div.classList.add('member');
+//     div.style.marginBlock=`5px`;
+//     div.style.background = `silver`;
+//
+//
+//     h2.innerText = `${simpson.name} - ${simpson.surname}`;
+//     i.innerText = `age ${simpson.age}`;
+//     p.innerText = `${simpson.info}`;
+//     img.setAttribute(`alt`, `${simpson.name}photo`);
+//     img.setAttribute(`src`, simpson.photo);
+//
+//
+//     div.append(h2,i,p,img);
+//     document.body.appendChild(div);
+// }
 
-    let div = document.createElement('div');
-    let h2 = document.createElement('h2');
-    let i = document.createElement('i');
-    let p = document.createElement('p');
-    let img = document.createElement('img');
 
+// Цикл в циклі
+// - Є масив coursesArray котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
 
-    div.classList.add('member');
-    div.style.marginBlock=`5px`;
-    div.style.background = `silver`;
+// Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами
+// Приклад структири знаходиться у файлі example.png який лежить в папці з поточним фaйлом
 
-
-    h2.innerText = `${simpson.name} - ${simpson.surname}`;
-    i.innerText = `age ${simpson.age}`;
-    p.innerText = `${simpson.info}`;
-    img.innerText = `${simpson.photo}`;
-
-    div.append(h2,i,p,img);
-    document.body.appendChild(div);
-}
-
-
-
-
-
+// let coursesArray = [
+//     {
+//         title: 'JavaScript Complex',
+//         monthDuration: 5,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+//     },
+//     {
+//         title: 'Java Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'java core',
+//             'java advanced']
+//     },
+//     {
+//         title: 'Python Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'python core',
+//             'python advanced']
+//     },
+//     {
+//         title: 'QA Complex',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+//     },
+//     {
+//         title: 'FullStack',
+//         monthDuration: 7,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'react',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'node.js',
+//             'python',
+//             'java']
+//     },
+//     {
+//         title: 'Frontend',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+//     }
+// ];
+//
+// const body = document.querySelector(`body`);
+// for (const item of coursesArray) {
+//     const block = document.createElement('div');
+//   const title = document.createElement('h1');
+//   const duration = document.createElement('div');
+//   const month = document.createElement('div');
+//   const hour = document.createElement('div');
+//   const ul = document.createElement('ul');
+//
+//     title.innerText = item.title;
+//   month.innerText = `monthDuration: ${item.monthDuration}`;
+//   hour.innerText = `hourDuration: ${item.hourDuration}`;
+//
+//     for (const module of item.modules) {
+//         const li = document.createElement(`li`);
+//         li.innerText = module;
+//         li.classList.add('blue', 'list-none');
+//         ul.appendChild(li);
+//     }
+//
+//     block.classList.add('main');
+//   title.classList.add('text-center', 'blue');
+//   duration.classList.add('flex');
+//   month.classList.add('blue', 'text-center', 'grow1');
+//   hour.classList.add('blue', 'text-center', 'grow5');
+//   ul.classList.add('margin0', 'blue')
+//
+//   duration.append(month,hour);
+//   block.append(title, duration, ul);
+//   body.append(block)
+// }
 
 
 
