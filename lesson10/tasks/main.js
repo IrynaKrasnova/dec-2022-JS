82006cf06a8e9c06f01ac9be5872154208b373dd
IrynaKrasnova <irene.ksn@gmail.com>
2023-03-13 07:55:18 +0200

    II

--- lesson10/tasks/main.js
+++ lesson10/tasks/main.js
@@ -19,3 +19,8 @@
 
 // є сторінка, на якій є блок, я кому знаходиьтся цифра. написати код, який при кожному перезавантажені сторінки буде додавати до неї +1
 
+let storinka = document.forms.pages;
+storinka.onclick =function (e) {
+    localStorage.getItem('pages')
+
+}
