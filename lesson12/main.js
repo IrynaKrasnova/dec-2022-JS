bc2305d7f6b17472e0952148ee6dcd32c0b0bfe7
IrynaKrasnova <irene.ksn@gmail.com>
2023-03-13 14:43:14 +0200

    III

--- lesson12/main.js
+++ lesson12/main.js
@@ -7,33 +7,32 @@ fetch(url)
             let wrapper = document.createElement('div');
             let id = document.createElement('div');
             let name = document.createElement('div');
-
+            let button = document.createElement('button')
 
 
             id.innerText = `${valueElement.id}`;
             name.innerText = `${valueElement.name}`;
+            button.innerText ='details';
 
             wrapper.classList.add('wrapper', 'item');
             id.classList.add('flex');
             name.classList.add('flex');
+            button.classList.add('button');
 
-
-
+            // button.onclick = function (){
+            //
+            // }
 
 
 
 
 
             // div.style.border = `5px solid darkred`;
-            //
-            //
-            //
-            //
             // div.style.marginBlock=`5px`;
             // div.style.background = `silver`;
 
 
-            wrapper.append(id,name)
+            wrapper.append(id,name,button);
             document.body.appendChild(wrapper);
 
         }
