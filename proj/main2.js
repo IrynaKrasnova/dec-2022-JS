75e81abcf245dccae40153f180dc84016eba0f26
IrynaKrasnova <irene.ksn@gmail.com>
2023-03-18 10:30:47 +0200

    base9

--- proj/main2.js
+++ proj/main2.js
@@ -1,3 +1,4 @@
+
 let url = new URL(location.href);
 let json = url.searchParams.get('data');
 let users = JSON.parse(json);
@@ -5,7 +6,7 @@ let users = JSON.parse(json);
 
 
 let block = document.createElement('div');
-let id = document.createElement('h2');
+let id = document.createElement('h4');
 let name = document.createElement('div');
 let username = document.createElement('div');
 let email = document.createElement('div');
@@ -18,7 +19,7 @@ let company = document.createElement('div');
 id.innerText =` ID: ${users.id}`;
 name.innerText = `Name: ${users.name}`;
 username.innerText = `Username: ${users.username}`;
-email.innerText = `e-mail: ${users.email}`;
+email.innerText = `E-mail: ${users.email}`;
 address.innerText = `Address: ${JSON.stringify(users.address)}`;
 phone.innerText = `Phone: ${users.phone}`
 website.innerText = `Website:${users.website}`;
@@ -32,27 +33,41 @@ document.body.appendChild(block);
 
 let url2 = new URL('https://jsonplaceholder.typicode.com/posts');
 
+// let url2 = new URL('https://jsonplaceholder.typicode.com/users/${userID}/posts');
+
+
 fetch(url2)
     .then(value => value.json())
     .then(posts => {
         for (const post of posts) {
 
 
+            let postOfCurrentUser = document.createElement('button');
+            // postOfCurrentUser.textContent = 'post of current user';
+            postOfCurrentUser.innerText = `${post.title}`
 
+            postOfCurrentUser.addEventListener('click', ()=>{
 
-            let a = document.createElement('a');
-            a.innerText = `${post.title}`;
-            a.href = 'post-details.html?id=' + JSON.stringify(post);
-            a.style.display = 'block'
+                // location.href = 'post-details.html?id=' + JSON.stringify(post)
+                location.href=`post-details.html?id=${post.id}` + JSON.stringify(post)
 
+                })
 
-
-            block.appendChild(a)
-
-
+            block.appendChild(postOfCurrentUser)
 
         }
 
     })
+
+
+
+
+
+
+
+
+
+
+
 
 
